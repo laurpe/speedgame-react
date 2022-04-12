fa00d85e8ee6ad6bbf1309f1502f280f02a9828e
laurpe <laura.perttila@edu.bc.fi>
2022-04-12 14:51:40 +0300

    popup finished

--- src/Components/Popup.jsx
+++ src/Components/Popup.jsx
@@ -1,10 +1,22 @@
 import styles from "./Popup.module.css";
 
 const Popup = ({ score, handleClick }) => {
+    let message = "";
+    if (score >= 40) {
+        message = "Amazing!";
+    } else if (score >= 25) {
+        message = "Great job!";
+    } else if (score >= 5) {
+        message = "Nice work!";
+    } else {
+        message = "Try a bit harder next time!";
+    }
+
     return (
         <div className={styles.popup}>
             <p>Your score was: {score}</p>
-            <button type="button" onClick={handleClick}>
+            <p>{message}</p>
+            <button type="button" className={styles.btn} onClick={handleClick}>
                 Close
             </button>
         </div>
--- src/Components/Score.jsx
+++ src/Components/Score.jsx
@@ -1,7 +1,7 @@
 import styles from "./Score.module.css";
 
 const Score = ({ score }) => {
-    return <div styles={styles.score}>Your score: {score}</div>;
+    return <div className={styles.score}>Your score: {score}</div>;
 };
 
 export default Score;
